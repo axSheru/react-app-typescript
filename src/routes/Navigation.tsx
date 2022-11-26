3418bb1a1c675df556065bf89cae4fd2d6744794
Alex Paredes <alexparedesr95@gmail.com>
2022-11-26 12:12:46 -0600

    FEAT: Implemented Suspense to show loading message.

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,43 +2,45 @@ import { BrowserRouter, Navigate, NavLink, Routes, Route } from "react-router-do
 import { routes } from "./routes";
 
 import logo from '../logo.svg';
-import { Component } from 'react';
+import { Suspense } from 'react';
 
 export const Navigation = () => {
     return (
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={ logo } alt="React logo" />
-                    <ul>
+        <Suspense fallback={ <span>Loading...</span> }>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={ logo } alt="React logo" />
+                        <ul>
+                            {
+                                // routes.map( route => ( // Se desestructura.
+                                routes.map( ({ to, name }) => (
+                                    <li key={ to }>
+                                        <NavLink
+                                            to={ to } 
+                                            className={ ({isActive}) => isActive ? 'nav-active' : '' }>
+                                                { name }
+                                        </NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
+                    <Routes>
                         {
-                            // routes.map( route => ( // Se desestructura.
-                            routes.map( ({ to, name }) => (
-                                <li key={ to }>
-                                    <NavLink
-                                        to={ to } 
-                                        className={ ({isActive}) => isActive ? 'nav-active' : '' }>
-                                            { name }
-                                    </NavLink>
-                                </li>
+                            // routes.map( route => ( //Se desestructura.
+                            routes.map(({ path, Component }) => (
+                                <Route
+                                    key={ path }
+                                    path={ path }
+                                    element={ <Component /> }
+                                />
                             ))
                         }
-                    </ul>
-                </nav>
-                <Routes>
-                    {
-                        // routes.map( route => ( //Se desestructura.
-                        routes.map(({ path, Component }) => (
-                            <Route
-                                key={ path }
-                                path={ path }
-                                element={ <Component /> }
-                            />
-                        ))
-                    }
-                    <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
-                </Routes>
-            </div>
-        </BrowserRouter>
+                        <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     );
 };
