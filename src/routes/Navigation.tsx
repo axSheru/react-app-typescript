68993c1d4cd87f9b9852bbcacd826483e2e35883
Alex Paredes <alexparedesr95@gmail.com>
2022-11-29 15:03:01 -0600

    FEAT: Created new ShoppingPage and ProductCard components.

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, NavLink, Routes, Route } from "react-router-dom";
+import { ShoppingPage } from "../02-components-pattern/pages/ShoppingPage";
 import logo from '../logo.svg';
 
 export const Navigation = () => {
@@ -9,7 +10,7 @@ export const Navigation = () => {
                     <img src={ logo } alt="React logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                            <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -20,7 +21,7 @@ export const Navigation = () => {
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="home" element={ <h1>Home page</h1> } />
+                    <Route path="home" element={ <ShoppingPage /> } />
                     <Route path="about" element={ <h1>About page</h1> } />
                     <Route path="users" element={ <h1>Users page</h1> } />
                     <Route path="/*" element={ <Navigate to="/home" replace /> } />
