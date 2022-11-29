3d5ee60d4db0d08cc8146e37b83df7b56a093bee
Alex Paredes <alexparedesr95@gmail.com>
2022-11-29 15:48:31 -0600

    FEAT: Passed ProductCard props.

--- src/02-components-pattern/pages/ShoppingPage.tsx
+++ src/02-components-pattern/pages/ShoppingPage.tsx
@@ -1,11 +1,25 @@
 import { ProductCard } from "../components/ProductCard";
 
+const product = {
+    id: '1',
+    title: 'Coffee Mug - Card',
+    image: './coffee-mug.png'
+};
+
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping store</h1>
             <hr />
-            <ProductCard />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap'
+                }}
+            >
+                <ProductCard product={ product }/>
+            </div>
         </div>
     );
 };
